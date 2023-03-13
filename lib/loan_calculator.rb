98745116c7766ea98e6bd6eae298228db249bdae
Andrew Owino <andystudy4@gmail.com>
2023-03-13 04:57:01 +0300

    Implement business logic

--- lib/loan_calculator.rb
+++ lib/loan_calculator.rb
@@ -1,14 +1,77 @@
 class LoanCalculator
-    attr_accessor :loan_amount, :loan_term_in_months, :interest_rate_per_year, :repayment_frequency
+    attr_accessor :principle, :loan_term_in_months, :interest_rate_per_year, :repayment_frequency
 
-    def initialize(loan_amount:, loan_term_in_months:, interest_rate_per_year:, repayment_frequency:)
-        @loan_amount = loan_amount.to_f
+    def initialize(principle:, loan_term_in_months:, interest_rate_per_year:, repayment_frequency:)
+        @principle = principle.to_f
         @loan_term_in_months = loan_term_in_months.to_i
         @interest_rate_per_year = interest_rate_per_year.to_f
         @repayment_frequency = repayment_frequency
     end
 
+    def calculate_repayments
 
+        case repayment_frequency
+            when "bi-monthly"
+                num_of_payments = @loan_term_in_months / 2
+                periodic_interest = ((interest_rate_per_year/100.0) / 12.0) * 2.0
+            when "monthly"
+                num_of_payments = @loan_term_in_months
+                periodic_interest = (interest_rate_per_year/100.0) / 12.0
+            when "weekly"
+                num_of_payments = @loan_term_in_months * 4
+                periodic_interest = ((interest_rate_per_year/100.0) / 12.0) / 4
+            end
+
+            repayment_amount = @principle / num_of_payments
+
+            repayments = [["principle", "interest_amount", "amount_paid", "balance"]]
+            total_payments = 0
+            total_interest_amount = 0
+            balance = Float::INFINITY
+            while num_of_payments >= 0 && balance > 0
+                installments = []
+                
+                installments << @principle
+                
+        
+                interest_amount = (@principle * periodic_interest)/100
+        
+                total_interest_amount += interest_amount
+
+                installments << interest_amount
+        
+                if repayment_amount > (interest_amount + balance)
+                    amount_paid = interest_amount + balance
+                else
+                    amount_paid = repayment_amount - interest_amount
+                end
+
+        
+                installments << amount_paid
+        
+                @principle -= amount_paid
+        
+                total_payments += amount_paid
+                
+                balance = @principle
+        
+                installments << balance
+                
+                repayments << installments
+        
+                num_of_payments-1
+            end
+        
+            print_table(repayments)
+    end
+
+    private
+    
+    def print_table(repayments)
+        repayments.each do |repayment|
+            puts "#{repayment[0]} | #{repayment[1]} | #{repayment[2]} | #{repayment[3]}"
+        end
+    end
+    
 end
-
 
