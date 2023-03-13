1fb50d7d6b1264075c8ff6ba1aa066c1fe2b080f
Andrew Owino <andystudy4@gmail.com>
2023-03-13 13:31:09 +0300

    Test with varying data of large inputs

--- spec/loan_repayment_calc_spec.rb
+++ spec/loan_repayment_calc_spec.rb
@@ -65,9 +65,9 @@ RSpec.describe LoanCalculator do
     end
 
     context 'with valid inputs' do
-      it 'calculates repayments correctly' do
-        calculator = LoanCalculator.new(principle: 1000, loan_term_in_months: 240, interest_rate_per_year: 6.5,
-                                        repayment_frequency: 'monthly')
+      it 'calculates repayments correctly even for large principle' do
+        calculator = LoanCalculator.new(principle: 10000000000, loan_term_in_months: 24, interest_rate_per_year: 12.67,
+                                        repayment_frequency: 'weekly')
         repayments = calculator.calculate_repayments
         expect(repayments.last[3]).to be_within(0.01).of(0.0)
       end
